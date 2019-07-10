25c0250cf47e3189e232b0e29fd8a702f2652972
fionavie <fionaarthur967@gmail.com>
2019-07-10 12:31:35 +0000

    first commit

---
+++ hello.py
@@ -0,0 +1,44 @@
+names = ["John", "Paul", "George", "Ringo"]
+for beatle in names:
+    print (beatle)
+    
+    
+print(type("hello"))
+
+print(type(3))
+
+
+
+
+print(type(b'hello'))
+
+
+names=["Fiona", "Richard", "Jane", "Julius"]
+print (names)
+
+
+names=["John", "Paul", "George", 3]
+print (names)
+
+
+yob = 2000
+now = 2019
+if (now-yob)< 18:
+    print ("you're still a child")
+elif (now-yob)< 25:
+   print ("whoa! still learning!")
+else:
+  print("old")
+
+
+yob =2000
+now =2019
+status = "young" if (now-yob)<30 else "old"
+print (status)
+
+
+names =["Gideon", "Richlove", "Emmanuella",]
+age =
+
+             
+             
